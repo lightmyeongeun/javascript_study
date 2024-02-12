9511a0e394da4e97ae51e92058b1f13c5911e6ff
lightmyeongeun <cme202908@naver.com>
2024-02-13 04:39:14 +0900

    last work

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ pickRandomNu()
 let playBtn=document.getElementById("play_button")
 let userInput=document.getElementById("user_input")
 let resultArea=document.getElementById("result_area")
+let resultAreaImg = document.querySelector(".main-img");//이미지 넣기
 
 playBtn.addEventListener("click",play)
 
@@ -22,23 +23,28 @@ function play(){
     let userValue=userInput.value;
 
     if(userValue<1 || userValue>100){
-        resultArea.textContent="1과 100사이 숫자를 입력해주세요"
+        resultArea.textContent="1~100까지 숫자를 선택해보세요!"
+        resultAreaImg.src ="images/retry.png";        
         return;
     }//유저가 1~100 범위 밖에 숫자를 입력시 알려준다. 기회를 감소시키지 않는다.
     
     if(history.includes(userValue)){
         resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요."
+        resultAreaImg.src ="images/same.png";        
         return      
     }//유저가 이미 입력한 숫자를 또 입력하면, 알려준다
 
     chances -- //5번의 기회 로직
     console.log("chance",chances)//5번의 기회 로직 : 기회 횟수 보이기
-    chanceArea.textContent=`남은기회 : ${chances}번`//5번의 기회 로직
+    chanceArea.textContent=`남은 기회 : ${chances}번`//5번의 기회 로직
     if(userValue<computerNum){
+        resultAreaImg.src ="images/going.png";        
         resultArea.textContent="Up!!!"
     }else if(userValue>computerNum){
+        resultAreaImg.src = "images/down.png";
         resultArea.textContent="Down!!!"
     }else{
+        resultAreaImg.src ="images/success.gif";
         resultArea.textContent="맞췄습니다!!!"
         gameOver=true
     }
@@ -68,10 +74,15 @@ function reset(){
     userInput.value=""
     //새로운 번호가 생성되고
     pickRandomNu()
-
-    resultArea.textContent="결과값이 여기 나옵니다!"
+    resultArea.textContent="1~100까지 숫자를 선택해보세요!"
+    chances = 10
+    gameOver = false
+    chanceArea.innerHTML = `남은 기회 : ${chances}번`
+    resultAreaImg.src =
+    "images/start.png";
 }
-//5번의 기회를 다 쓰면 게임이 끝난다.(더이상 추측 불가, 버튼이 disabled)
+
+//10번의 기회를 다 쓰면 게임이 끝난다.(더이상 추측 불가, 버튼이 disabled)
 let chances=10
 let gameOver=false
 let chanceArea=document.getElementById("chance_area")
@@ -79,6 +90,4 @@ let chanceArea=document.getElementById("chance_area")
 let history=[]
 
 // 유저가 input창에 입력시에 새로운 칸으로 노출시키기
-userInput.addEventListener("focus",function()
-    {userInput.value=""
-})
+userInput.addEventListener("focus",function(){userInput.value=""})
